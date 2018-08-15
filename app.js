72667f6e780bd63df939b9cce0bb90ce43b4452b
jbromley94 <jordanbromley94@gmail.com>
2018-08-15 11:28:27 +0100

    grabs all the names of pathways and serves them in an array

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const https = require("https");
 
-https.get("https://web-crawler-test2.herokuapp.com/", (response) => {
-  let data='';
+const recursFinder = (URL) =>{https
+  .get(
+    "https://web-crawler-test2.herokuapp.com",
+    response => {
+      let data = "";
 
-  response.on("data", (information) => {
-    data+= information;
-  });
+      response.on("data", information => {
+        data += information;
+      });
 
-response.on("end", () =>{
-  console.log(data);
-})
-}).on("error", (err) =>{
-  console.log("Error: "+ err.message)
-});
+      response.on("end", () => {
+        dataArr = data.split("public/");
+        let html = ".html>";
+        let allHtml = dataArr
+          .filter(item => {
+            if (item.includes(html)) {
+              return item;
+            }
+          })
+          .map(item => {
+            let res = item.split(">");
+            return res[0];
+          });
+        console.log(allHtml)
+      });
+    }
+  )
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });}
