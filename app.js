fc50851c6ca55858640ad8df115a4807cfb3c0f2
jbromley94 <jordanbromley94@gmail.com>
2018-08-15 14:26:31 +0100

    the function is not yet recursive

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const https = require("https");
 let URL = "https://web-crawler-test2.herokuapp.com";
 let count=0
 let pathsToSearch= []
+let lastTier=''
+let searchedTerms=[]
 
 
 const recursFinder = (URL) =>{
@@ -27,9 +29,34 @@ const recursFinder = (URL) =>{
             let res = item.split(">");
             return {pathNames : res[0]};
           });
+        
         pathsToSearch = allHtml
-        if(allHtml.length===0){count++}
-        let firstSearch = false;
+        
+        
+        for (let i=0; i<pathsToSearch.length; i++){
+          console.log(pathsToSearch[i].pathNames, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+          // searchedTerms.push(pathsToSearch[i].pathNames);
+          // console.log(">>>>>>>>>>>>>>>>>>>>>>.",searchedTerms)
+          // searchedTerms.forEach(path => {
+          //   if (path === pathsToSearch[i].pathNames){
+          //     i++
+          //   }
+          // })
+          console.log(pathsToSearch)
+
+          if(response.statusCode === 404) {
+            console.log(`here`)
+            return recursFinder(`${URL}/public/${searchedTerms[0][i+1].pathNames}`);
+          }
+          lastTier = pathsToSearch[i].pathNames
+         return recursFinder(`${URL}/public/${pathsToSearch[i].pathNames}`)
+        }
+
+        if (allHtml.length === 0) {
+          console.log(lastTier);
+          return recursFinder(`${URL}/public/${searchedTerms[0]}`);
+        }
+        
         console.log(pathsToSearch)
       });
     }
@@ -40,3 +67,5 @@ const recursFinder = (URL) =>{
 
 
   recursFinder(URL)
+
+module.exports={recursFinder}
